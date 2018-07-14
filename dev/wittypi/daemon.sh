bbe9ba1b13aae4aec79bd80d6c31399d3e65aa42
JC <john.doe@example.com>
2018-07-14 20:15:52 +0100

    moved wittpi dev stuff around.

---
+++ dev/wittypi/daemon.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# file: daemon.sh
+#
+# This script should be auto started, to support WittyPi hardware
+#
+
+# check if sudo is used
+if [ "$(id -u)" != 0 ]; then
+  echo 'Sorry, you need to run this script with sudo'
+  exit 1
+fi
+
+# get current directory
+cur_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# utilities
+. "$cur_dir/utilities.sh"
+
+log 'Witty Pi daemon (v2.56) is started.'
+
+# halt by GPIO-4 (BCM naming)
+halt_pin=4
+
+# make sure the halt pin is input with internal pull up
+gpio -g mode $halt_pin up
+gpio -g mode $halt_pin in
+
+# LED on GPIO-17 (BCM naming)
+led_pin=17
+
+# wait for RTC ready
+sleep 2
+
+# if RTC presents
+is_rtc_connected
+has_rtc=$?  # should be 0 if RTC presents
+
+if [ $has_rtc == 0 ] ; then
+  # disable square wave and enable alarms
+  i2c_write 0x01 0x68 0x0E 0x07
+
+  byte_F=$(i2c_read 0x01 0x68 0x0F)
+
+  # if woke up by alarm B (shutdown), turn it off immediately
+  if [ $((($byte_F&0x1) == 0)) == '1' ] && [ $((($byte_F&0x2) != 0)) == '1' ] ; then
+    log 'Seems I was unexpectedly woken up by shutdown alarm, must go back to sleep...'
+    do_shutdown $halt_pin $led_pin $has_rtc
+  fi
+
+  # clear alarm flags
+  clear_alarm_flags $byte_F
+else
+  log 'Witty Pi is not connected, skip I2C communications...'
+fi
+
+# synchronize time
+if [ $has_rtc == 0 ] ; then
+  "$cur_dir/syncTime.sh" &
+else
+  log 'Witty Pi is not connected, skip synchronizing time...'
+fi
+
+# wait for system time update
+sleep 3
+
+# run schedule script
+if [ $has_rtc == 0 ] ; then
+  "$cur_dir/runScript.sh" 0 revise >> "$cur_dir/schedule.log" &
+else
+  log 'Witty Pi is not connected, skip schedule script...'
+fi
+
+# delay until GPIO pin state gets stable
+counter=0
+while [ $counter -lt 5 ]; do  # increase this value if it needs more time
+  if [ $(gpio -g read $halt_pin) == '1' ] ; then
+    counter=$(($counter+1))
+  else
+    counter=0
+  fi
+  sleep 1
+done
+
+# run extra tasks in background
+"$cur_dir/extraTasks.sh" >> "$cur_dir/wittyPi.log" 2>&1 &
+
+# wait for GPIO-4 (BCM naming) falling, or alarm B (shutdown)
+log 'Pending for incoming shutdown command...'
+while true; do
+  gpio -g wfi $halt_pin falling
+  if [ $has_rtc == 0 ] ; then
+    byte_F=$(i2c_read 0x01 0x68 0x0F)
+    if [ $((($byte_F&0x1) != 0)) == '1' ] && [ $((($byte_F&0x2) == 0)) == '1' ] ; then
+      # alarm A (startup) occurs, clear flags and ignore
+      log 'Startup alarm occurs in ON state, ignored'
+      clear_alarm_flags
+    else
+      break;
+    fi
+  else
+    # power switch can still work without RTC
+    break;
+  fi
+done
+log 'Shutdown command is received...'
+
+do_shutdown $halt_pin $led_pin $has_rtc
---
+++ dev/wittypi/wp.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# must be root
+if [ "$(id -u)" != 0 ]; then
+  echo 'Sorry, you need to run this script with sudo'
+  exit 1
+fi
+
+# include utilities script in same directory
+my_dir_tmp="`dirname \"$0\"`"
+my_dir="`( cd \"$my_dir_tmp\" && pwd )`"
+if [ -z "$my_dir" ] ; then
+  exit 1
+fi
+. $my_dir/utilities.sh
+
+# unload the rtc from /dev if it's there.
+if [ -e /dev/rtc ] ; then
+  unload_rtc
+fi
+
+
+JCbounce()
+{
+    echo "JCBounce() here: lets bounce back in 1 minute:"
+
+    # get the datetime in 2 minutes in UTC
+    rbt=$(date -u -d "1 minute" +"%d %H:%M:%S")
+    IFS=' ' read -r date timestr <<< "$rbt"
+    IFS=':' read -r hour minute second <<< "$timestr"
+
+    echo "Bounce: will be back on: $date at: $hour:$minute:$second (UTC)"
+    
+    set_startup_time $date $hour $minute $second
+    
+    do_shutdown 4 17 0
+}
+
+reset_startup_time()
+{
+    log '  Clearing auto startup time...' '-n'
+    clear_startup_time
+    log ' done :-)'
+}
+
+reset_shutdown_time()
+{
+    log '  Clearing auto shutdown time...' '-n'
+    clear_shutdown_time
+    log ' done :-)'
+}
+
+reset_all()
+{
+    reset_startup_time
+    reset_shutdown_time
+    delete_schedule_script
+}
+
+reset_data()
+{
+    echo 'Here you can reset some data:'
+    echo '  [1] Clear scheduled startup time'
+    echo '  [2] Clear scheduled shutdown time'
+    echo '  [3] Stop using schedule script'
+    echo '  [4] Perform all actions above'
+    read -p "Which action to perform? (1~4) " action
+    case $action in
+        [1]* ) reset_startup_time;;
+        [2]* ) reset_shutdown_time;;
+        [3]* ) delete_schedule_script;;
+        [4]* ) reset_all;;
+        * ) echo 'Please choose from 1 to 4';;
+    esac
+}
+
+
+# MAIN loop is below, but if command line arg is "bounce" then bounce.
+
+if [ "$1" == "bounce" ] ; then
+    JCbounce
+elif [ "$1" == "status" ] ; then
+    systime='>>> Your system time is: '
+    systime+="$(get_sys_time)"
+    echo "$systime"
+
+    # output RTC time
+    rtctime='>>> Your RTC time is:    '
+    rtctime+="$(get_rtc_time)"
+    echo "$rtctime"
+
+    sut=$(get_startup_time) # ; supl=$(get_local_date_time "$sdt")
+    echo "INFO:. Schedule next startup [$sut]"
+    exit
+else    
+    echo "command line not recognised. (try: status, bounce)"
+    exit
+fi
+
+echo "Fell through the if loop... exiting"
+exit

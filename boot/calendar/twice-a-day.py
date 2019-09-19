68b2ed4a72e02616e5bc44c69458ef48863fcf5b
JC <john.doe@example.com>
2019-09-19 14:02:53 +0100

    updated

--- boot/calendar/twice-a-day.py
+++ boot/calendar/twice-a-day.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
-import datetime
-import sys
+# This file is a Solo Calendar
+# This file is part of amon (https://github.com/solo-system/amon.git)
+# see readme.txt for info on calendars
 
-t = datetime.datetime.now()
-# t = datetime.datetime(2018, 1, 1, 22, 39, 0, 0)
-
-#print(t)
-
+# This calendar records two periods per day.
 # split the day into 5 sections:
 # 0000 -> 0430 OFF (awake at 0430)
 # 0430 -> 0630 ON  
@@ -15,19 +12,63 @@ t = datetime.datetime.now()
 # 1800 -> 2000 ON
 # 2000 -> 0000 OFF (awake at 0430 TOMORROW)
 
-if ( t.time() < datetime.time(4, 30) ):
-#   print("DEBUG: session 1 - off until 0430")
-   print("off {} {} {} {} {} {}".format(t.year, t.month, t.day, 4, 30, 0))
-elif ( t.time() < datetime.time(6, 30) ):
-#   print("DEBUG: session 2 - on until 0630")
-   print("on")
-elif ( t.time() < datetime.time(18, 0) ):
-#   print("DEBUG: session 3 - off until 1800")
-   print("off {} {} {} {} {} {}".format(t.year, t.month, t.day, 18, 0, 0))
-elif ( t.time() < datetime.time(20, 0)  ):
-#   print("DEBUG: session 4 - on until 2000")
-   print("on")
-else: # it's after 2000 
-#   print("DEBUG: session 5 - off until 0430 tomorrow")
-   tomorrow = t + datetime.timedelta(days=1)
-   print("off {} {} {} {} {} {}".format(tomorrow.year, tomorrow.month, tomorrow.day, 4, 30, 0))
+import sys
+import datetime
+import pytz
+
+# what is the output format we hand back to amon?
+returnformat="%Y %m %d %H %M %S" # returned on stdout as the reboot time
+fmt = '%Y-%m-%d %H:%M:%S %Z%z' # mostly for debug
+
+# get NOW in UTC - and ensure the datetime object is AWARE it's a UTC timezone (not naive).
+naive_utc_now = datetime.datetime.utcnow()
+
+# make an aware version of the above - so it KNOWS it's in UTC.
+aware_utc_now = pytz.utc.localize(naive_utc_now)
+
+# get a represenataion of NOW aware of local (system) timezone:
+now = aware_utc_now.astimezone()
+
+print("\ntwice-a-day.py (time-now: {})".format(now.strftime(fmt)), file=sys.stderr)
+
+if ( now.time() < datetime.time(4, 30) ):
+   t = now.replace(hour=4, minute=30, second=0)
+   # is t naive?
+   rbt = t.astimezone(pytz.utc)
+   rs = "off {}".format(rbt.strftime(returnformat))
+   print(rs)
+   print("twice-a-day.py: session 1 (0000->0430): off rbt= (0430) {}".format(rbt), file=sys.stderr)
+   print("twice-a-day.py: rebooting in {}".format(rbt-aware_utc_now), file=sys.stderr)
+   print("twice-a-day.py: returning: {}".format(rs), file=sys.stderr)
+
+elif ( now.time() < datetime.time(6, 30) ):
+   rs = "on"
+   print(rs)
+   print("twice-a-day.py: session 2 (0430->0630): on", file=sys.stderr)
+   print("twice-a-day.py: returning: {}".format(rs), file=sys.stderr)
+
+elif ( now.time() < datetime.time(18, 0) ):
+   t = now.replace(hour=18, minute=0, second=0)
+   rbt = t.astimezone(pytz.utc)
+   rs = "off {}".format(rbt.strftime(returnformat))
+   print(rs)
+   print("twice-a-day.py: session 3 (0630->1800): off. rbt= (1800) {}".format(rbt), file=sys.stderr)
+   print("twice-a-day.py: rebooting in {}".format(rbt-aware_utc_now), file=sys.stderr)
+   print("twice-a-day.py: returning: {}".format(rs), file=sys.stderr)
+
+elif ( now.time() < datetime.time(20, 0)  ):
+   rs = "on"
+   print(rs)
+   print("twice-a-day.py: session 4 (between 1800 and 2000) - on", file=sys.stderr)
+   print("twice-a-day.py: returning: {}".format(rs), file=sys.stderr)
+
+else:
+   t = now + datetime.timedelta(days=1) # skip one day into the future (to get DATE right)
+   t = t.replace(hour=4,minute=30,second=0) # fix up the time for restart tomorrow.
+   rbt = t.astimezone(pytz.utc)
+   rs = "off {}".format(rbt.strftime(returnformat))
+   print(rs)
+   print("twice-a-day.py: session 5 (after 2000) - off - reboot at 0430 tomorrow", file=sys.stderr)
+   print("twice-a-day.py: rebooting in {}".format(rbt-aware_utc_now), file=sys.stderr)
+   print("twice-a-day.py: returning: {}".format(rs), file=sys.stderr)
+
